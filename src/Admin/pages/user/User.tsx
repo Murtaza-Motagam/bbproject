525dbd801a2e28d455b84b44dd3cee498eb62ff4
Murtaza Motagam <qasimam89@gmail.com>
2024-02-07 13:59:22 +0530

    Admin Panel Creation Step-1

---
+++ src/Admin/pages/user/User.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Single from "../../components/single/Single"
+import { singleUser } from "../../data"
+import "./user.scss"
+
+const User = () => {
+
+  //Fetch data and send to Single Component
+  
+  return (
+    <div className="user">
+      <Single {...singleUser}/>
+    </div>
+  )
+}
+
+export default User
